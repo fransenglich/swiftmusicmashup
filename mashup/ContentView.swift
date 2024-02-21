00916c4655779ecb9e52c3df0b4f89b07602cad5
Frans Englich <fenglich@fastmail.fm>
2024-02-21 10:14:56 +0100

    Initial Commit

---
+++ mashup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mashup
+//
+//  Created by Frans Englich on 2024-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mashup/mashupApp.swift
@@ -0,0 +1,17 @@
+//
+//  mashupApp.swift
+//  mashup
+//
+//  Created by Frans Englich on 2024-02-21.
+//
+
+import SwiftUI
+
+@main
+struct mashupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
